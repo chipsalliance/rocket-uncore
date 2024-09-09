d308d1cbe7ea9b0d677365cdc371d3903afc96eb
Jiuyang Liu <liu@jiuyang.me>
2024-09-09 15:11:39 +0800

    [aclint] prototype aclint with MTIMER

--- aclint/src/ACLINT.scala
+++ aclint/src/ACLINT.scala
@@ -4,10 +4,14 @@
 package org.chipsalliance.uncore.aclint
 
 import chisel3._
+import chisel3.experimental.dataview.DataViewable
 import chisel3.experimental.hierarchy.{instantiable, Instance, Instantiate}
 import chisel3.experimental.{SerializableModule, SerializableModuleParameter}
 import chisel3.probe.{define, Probe, ProbeValue}
 import chisel3.properties.{AnyClassType, Class, Property}
+import org.chipsalliance.amba.RegMapper.regmap
+import org.chipsalliance.amba._
+import org.chipsalliance.amba.axi4.bundle._
 
 object ACLINTParameter {
   implicit def rwP: upickle.default.ReadWriter[ACLINTParameter] =
@@ -15,7 +19,17 @@ object ACLINTParameter {
 }
 
 /** Parameter of [[ACLINT]] */
-case class ACLINTParameter(useAsyncReset: Boolean) extends SerializableModuleParameter
+case class ACLINTParameter(
+                            useAsyncReset:  Boolean,
+                            mtimeBase:      BigInt,
+                            mtimecmpBase:   BigInt,
+                            harts:          Int,
+                            satelliteMtime: Boolean,
+                            axi4parameter:  AXI4BundleParameter)
+  extends SerializableModuleParameter {
+  require(!satelliteMtime, "unimpl")
+  require(axi4parameter.isRW)
+}
 
 /** Verification IO of [[ACLINT]] */
 class ACLINTProbe(parameter: ACLINTParameter) extends Bundle {}
@@ -33,12 +47,18 @@ class ACLINTInterface(parameter: ACLINTParameter) extends Bundle {
   val reset = Input(if (parameter.useAsyncReset) AsyncReset() else Bool())
   val probe = Output(Probe(new ACLINTProbe(parameter), layers.Verification))
   val om = Output(Property[AnyClassType]())
+  // TODO: do we really need two axi ports for two register groups? I'm sure.
+  val mtimerTime =
+    Flipped(axi4.bundle.verilog.irrevocable(parameter.axi4parameter)).asInstanceOf[AXI4RWIrrevocableVerilog]
+  val mtimerCompare =
+    Flipped(axi4.bundle.verilog.irrevocable(parameter.axi4parameter)).asInstanceOf[AXI4RWIrrevocableVerilog]
+  val mip = Output(Vec(parameter.harts, Bool()))
 }
 
 /** Hardware Implementation of ACLINT */
 @instantiable
 class ACLINT(val parameter: ACLINTParameter)
-    extends FixedIORawModule(new ACLINTInterface(parameter))
+  extends FixedIORawModule(new ACLINTInterface(parameter))
     with SerializableModule[ACLINTParameter]
     with ImplicitClock
     with ImplicitReset {
@@ -53,4 +73,44 @@ class ACLINT(val parameter: ACLINTParameter)
   // Assign Metadata
   val omInstance: Instance[ACLINTOM] = Instantiate(new ACLINTOM(parameter))
   io.om := omInstance.getPropertyReference.asAnyClassType
+  // 2. Machine-level Timer Device (MTIMER)
+  /** MTIME Register (Offset: 0x00000000) The MTIME register is a 64-bit read-write register that contains the number of
+   * cycles counted based on a fixed reference frequency. On MTIMER device reset, the MTIME register is cleared to
+   * zero.
+   */
+  val mtime = RegInit(0.U(64.W))
+
+  regmap(
+    io.mtimerTime.viewAs[AXI4RWIrrevocable],
+    0,
+    false,
+    0 -> RegField.bytes(mtime, Some(RegFieldDesc("MTIME", "Machine-level time counter")))
+  )
+
+  /** MTIMECMP Registers (Offsets: 0x00000000 - 0x00007FF0) The MTIMECMP registers are per-HART 64-bit read-write
+   * registers. It contains the MTIME register value at which machine-level timer interrupt is to be triggered for the
+   * corresponding HART. The machine-level timer interrupt of a HART is pending whenever MTIME is greater than or equal
+   * to the value in the corresponding MTIMECMP register whereas the machine-level timer interrupt of a HART is cleared
+   * whenever MTIME is less than the value of the corresponding MTIMECMP register. The machine-level timer interrupt is
+   * reflected in the MTIP bit of the mip CSR. On MTIMER device reset, the MTIMECMP registers are in unknown state.
+   */
+  val mtimecmp = Seq.tabulate(parameter.harts)(idx => Reg(UInt(64.W)).suggestName(s"mtimecmp_${idx}"))
+
+  regmap(
+    io.mtimerCompare.viewAs[AXI4RWIrrevocable],
+    0,
+    false,
+    mtimecmp.zipWithIndex.map { case (reg, idx) =>
+      idx -> RegField.bytes(
+        reg,
+        Some(RegFieldDesc(s"MTIMECMP$idx", s"HART index $idx machine-level time compare", reset = None))
+      )
+    }: _*
+  )
+
+  io.mip := VecInit(mtimecmp.map(_ >= mtime))
+
+  // 3. Machine-level Software Interrupt Device (MSWI)
+
+  // 4. Supervisor-level Software Interrupt Device (SSWI)
 }
