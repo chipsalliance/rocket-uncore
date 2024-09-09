d0389b9474536576e6a40761a7c3f12c3fba539b
Jiuyang Liu <liu@jiuyang.me>
2024-09-09 20:24:45 +0800

    [plic] prototype plic interface

--- plic/src/PLIC.scala
+++ plic/src/PLIC.scala
@@ -1,13 +1,17 @@
 // SPDX-License-Identifier: Apache-2.0
+// SPDX-FileCopyrightText: 2016-2017 SiFive, Inc
 // SPDX-FileCopyrightText: 2024 Jiuyang Liu <liu@jiuyang.me>
 
 package org.chipsalliance.uncore.plic
 
 import chisel3._
-import chisel3.experimental.hierarchy.{instantiable, Instance, Instantiate}
+import chisel3.experimental.hierarchy.{Instance, Instantiate, instantiable}
 import chisel3.experimental.{SerializableModule, SerializableModuleParameter}
-import chisel3.probe.{define, Probe, ProbeValue}
+import chisel3.probe.{Probe, ProbeValue, define}
 import chisel3.properties.{AnyClassType, Class, Property}
+import chisel3.util.log2Ceil
+import org.chipsalliance.amba._
+import org.chipsalliance.amba.axi4.bundle._
 
 object PLICParameter {
   implicit def rwP: upickle.default.ReadWriter[PLICParameter] =
@@ -15,7 +19,23 @@ object PLICParameter {
 }
 
 /** Parameter of [[PLIC]] */
-case class PLICParameter(useAsyncReset: Boolean) extends SerializableModuleParameter
+case class PLICParameter(useAsyncReset: Boolean,
+                         base: BigInt,
+                         harts: Int,
+                         sources: Int,
+                         maxPriorities: Int,
+                         axi4parameter: AXI4BundleParameter,
+                        ) extends SerializableModuleParameter {
+  def nDevices: Int = sources
+
+  def minPriorities = math.min(maxPriorities, nDevices)
+
+  def nPriorities = (1 << log2Ceil(minPriorities + 1)) - 1 // round up to next 2^n-1
+
+  val prioBits = log2Ceil(nPriorities + 1)
+
+  def nHarts = harts
+}
 
 /** Verification IO of [[PLIC]] */
 class PLICProbe(parameter: PLICParameter) extends Bundle {}
@@ -33,12 +53,16 @@ class PLICInterface(parameter: PLICParameter) extends Bundle {
   val reset = Input(if (parameter.useAsyncReset) AsyncReset() else Bool())
   val probe = Output(Probe(new PLICProbe(parameter), layers.Verification))
   val om = Output(Property[AnyClassType]())
+  val plic = Flipped(axi4.bundle.verilog.irrevocable(parameter.axi4parameter)).asInstanceOf[AXI4RWIrrevocableVerilog]
+  val source = Input(Vec(parameter.sources, Bool()))
+  val mip = Output(Vec(parameter.harts, Bool()))
+  val sip = Output(Vec(parameter.harts, Bool()))
 }
 
 /** Hardware Implementation of PLIC */
 @instantiable
 class PLIC(val parameter: PLICParameter)
-    extends FixedIORawModule(new PLICInterface(parameter))
+  extends FixedIORawModule(new PLICInterface(parameter))
     with SerializableModule[PLICParameter]
     with ImplicitClock
     with ImplicitReset {
@@ -53,6 +77,39 @@ class PLIC(val parameter: PLICParameter)
   // Assign Metadata
   val omInstance: Instance[PLICOM] = Instantiate(new PLICOM(parameter))
   io.om := omInstance.getPropertyReference.asAnyClassType
+
+  def priorityRegDesc(i: Int) =
+    RegFieldDesc(
+      name = s"priority_$i",
+      desc = s"Acting priority of interrupt source $i",
+      group = Some(s"priority_${i}"),
+      groupDesc = Some(s"Acting priority of interrupt source $i"),
+      reset = if (parameter.nPriorities > 0) None else Some(1))
+
+  def pendingRegDesc(i: Int) =
+    RegFieldDesc(
+      name = s"pending_$i",
+      desc = s"Set to 1 if interrupt source $i is pending, regardless of its enable or priority setting.",
+      group = Some("pending"),
+      groupDesc = Some("Pending Bit Array. 1 Bit for each interrupt source."),
+      volatile = true)
+
+  def enableRegDesc(i: Int, j: Int, wide: Int) = {
+    val low = if (j == 0) 1 else j * 8
+    val high = low + wide - 1
+    RegFieldDesc(
+      name = s"enables_${j}",
+      desc = s"Targets ${low}-${high}. Set bits to 1 if interrupt should be enabled.",
+      group = Some(s"enables_${i}"),
+      groupDesc = Some(s"Enable bits for each interrupt source for target $i. 1 bit for each interrupt source."))
+  }
+
+  def thresholdRegDesc(i: Int) =
+    RegFieldDesc(
+      name = s"threshold_$i",
+      desc = s"Interrupt & claim threshold for target $i. Maximum value is ${parameter.nPriorities}.",
+      reset    = if (parameter.nPriorities > 0) None else Some(1))
+
 }
 
 object PLICTestBenchParameter {
