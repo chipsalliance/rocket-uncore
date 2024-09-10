bf9743b95a08df4d35f8272569d40447fe782bc6
Lucas-Wye <lustrew@foxmail.com>
2024-09-10 05:32:46 +0000

    [aclint] full implement

--- aclint/src/ACLINT.scala
+++ aclint/src/ACLINT.scala
@@ -20,13 +20,13 @@ object ACLINTParameter {
 
 /** Parameter of [[ACLINT]] */
 case class ACLINTParameter(
-                            useAsyncReset:  Boolean,
-                            mtimeBase:      BigInt,
-                            mtimecmpBase:   BigInt,
-                            harts:          Int,
-                            satelliteMtime: Boolean,
-                            axi4parameter:  AXI4BundleParameter)
-  extends SerializableModuleParameter {
+  useAsyncReset:  Boolean,
+  mtimeBase:      BigInt,
+  mtimecmpBase:   BigInt,
+  harts:          Int,
+  satelliteMtime: Boolean,
+  axi4parameter:  AXI4BundleParameter)
+    extends SerializableModuleParameter {
   require(!satelliteMtime, "unimpl")
   require(axi4parameter.isRW)
 }
@@ -52,13 +52,19 @@ class ACLINTInterface(parameter: ACLINTParameter) extends Bundle {
     Flipped(axi4.bundle.verilog.irrevocable(parameter.axi4parameter)).asInstanceOf[AXI4RWIrrevocableVerilog]
   val mtimerCompare =
     Flipped(axi4.bundle.verilog.irrevocable(parameter.axi4parameter)).asInstanceOf[AXI4RWIrrevocableVerilog]
+  val mswiDevice =
+    Flipped(axi4.bundle.verilog.irrevocable(parameter.axi4parameter)).asInstanceOf[AXI4RWIrrevocableVerilog]
+  val sswiDevice =
+    Flipped(axi4.bundle.verilog.irrevocable(parameter.axi4parameter)).asInstanceOf[AXI4RWIrrevocableVerilog]
   val mip = Output(Vec(parameter.harts, Bool()))
+  val tip = Output(Vec(parameter.harts, Bool()))
+  val sip = Output(Vec(parameter.harts, Bool()))
 }
 
-/** Hardware Implementation of ACLINT */
+/** Hardware Implementation of ACLINT [https://github.com/riscv/riscv-aclint/blob/main/riscv-aclint.adoc] */
 @instantiable
 class ACLINT(val parameter: ACLINTParameter)
-  extends FixedIORawModule(new ACLINTInterface(parameter))
+    extends FixedIORawModule(new ACLINTInterface(parameter))
     with SerializableModule[ACLINTParameter]
     with ImplicitClock
     with ImplicitReset {
@@ -75,9 +81,9 @@ class ACLINT(val parameter: ACLINTParameter)
   io.om := omInstance.getPropertyReference.asAnyClassType
   // 2. Machine-level Timer Device (MTIMER)
   /** MTIME Register (Offset: 0x00000000) The MTIME register is a 64-bit read-write register that contains the number of
-   * cycles counted based on a fixed reference frequency. On MTIMER device reset, the MTIME register is cleared to
-   * zero.
-   */
+    * cycles counted based on a fixed reference frequency. On MTIMER device reset, the MTIME register is cleared to
+    * zero.
+    */
   val mtime = RegInit(0.U(64.W))
 
   regmap(
@@ -88,12 +94,12 @@ class ACLINT(val parameter: ACLINTParameter)
   )
 
   /** MTIMECMP Registers (Offsets: 0x00000000 - 0x00007FF0) The MTIMECMP registers are per-HART 64-bit read-write
-   * registers. It contains the MTIME register value at which machine-level timer interrupt is to be triggered for the
-   * corresponding HART. The machine-level timer interrupt of a HART is pending whenever MTIME is greater than or equal
-   * to the value in the corresponding MTIMECMP register whereas the machine-level timer interrupt of a HART is cleared
-   * whenever MTIME is less than the value of the corresponding MTIMECMP register. The machine-level timer interrupt is
-   * reflected in the MTIP bit of the mip CSR. On MTIMER device reset, the MTIMECMP registers are in unknown state.
-   */
+    * registers. It contains the MTIME register value at which machine-level timer interrupt is to be triggered for the
+    * corresponding HART. The machine-level timer interrupt of a HART is pending whenever MTIME is greater than or equal
+    * to the value in the corresponding MTIMECMP register whereas the machine-level timer interrupt of a HART is cleared
+    * whenever MTIME is less than the value of the corresponding MTIMECMP register. The machine-level timer interrupt is
+    * reflected in the MTIP bit of the mip CSR. On MTIMER device reset, the MTIMECMP registers are in unknown state.
+    */
   val mtimecmp = Seq.tabulate(parameter.harts)(idx => Reg(UInt(64.W)).suggestName(s"mtimecmp_${idx}"))
 
   regmap(
@@ -108,9 +114,71 @@ class ACLINT(val parameter: ACLINTParameter)
     }: _*
   )
 
-  io.mip := VecInit(mtimecmp.map(_ >= mtime))
+  io.tip := VecInit(mtimecmp.map(_ >= mtime))
 
   // 3. Machine-level Software Interrupt Device (MSWI)
+  /** The MSWI device provides machine-level IPI functionality for a set of HARTs on a RISC-V platform. It has an IPI
+    * register (MSIP) for each HART connected to the MSWI device.
+    *
+    * On a RISC-V platform with multiple MSWI devices, each MSWI device provides machine-level IPI functionality for a
+    * different (or disjoint) set of HARTs. A MSWI device assigns a HART index starting from zero to each HART
+    * associated with it. The HART index assigned to a HART by the MSWI device may or may not have any relationship with
+    * the unique HART identifier (hart ID) that the RISC-V Privileged Architecture assigns to the HART.
+    *
+    * The maximum number of HARTs supported by a single MSWI device is 4095 which is equivalent to the maximum number of
+    * MSIP registers.
+    */
+  val msip = Seq.tabulate(parameter.harts)(idx => Reg(UInt(32.W)).suggestName(s"mswi_${idx}"))
+  regmap(
+    io.mswiDevice.viewAs[AXI4RWIrrevocable],
+    0,
+    false,
+    msip.zipWithIndex.map { case (reg, idx) =>
+      idx -> RegField.bytes(
+        reg,
+        Some(RegFieldDesc(s"MSWI$idx", s"HART index $idx machine-level software interrupt device", reset = None))
+      )
+    }: _*
+  )
+
+  /** Each MSIP register is a 32-bit wide WARL register where the upper 31 bits are wired to zero. The least significant
+    * bit is reflected in MSIP of the mip CSR. A machine-level software interrupt for a HART is pending or cleared by
+    * writing 1 or 0 respectively to the corresponding MSIP register.
+    *
+    * On MSWI device reset, each MSIP register is cleared to zero.
+    */
+  io.mip := VecInit(msip.map(s => s(0).asBool))
 
   // 4. Supervisor-level Software Interrupt Device (SSWI)
+  /** The SSWI device provides supervisor-level IPI functionality for a set of HARTs on a RISC-V platform. It provides a
+    * register to set an IPI (SETSSIP) for each HART connected to the SSWI device.
+    *
+    * On a RISC-V platform with multiple SSWI devices, each SSWI device provides supervisor-level IPI functionality for
+    * a different (or disjoint) set of HARTs. A SSWI device assigns a HART index starting from zero to each HART
+    * associated with it. The HART index assigned to a HART by the SSWI device may or may not have any relationship with
+    * the unique HART identifier (hart ID) that the RISC-V Privileged Architecture assigns to the HART.
+    *
+    * The maximum number of HARTs supported by a single SSWI device is 4095 which is equivalent to the maximum number of
+    * SETSSIP registers.
+    */
+  val setssip = Seq.tabulate(parameter.harts)(idx => Reg(UInt(32.W)).suggestName(s"sswi_${idx}"))
+  regmap(
+    io.sswiDevice.viewAs[AXI4RWIrrevocable],
+    0,
+    false,
+    setssip.zipWithIndex.map { case (reg, idx) =>
+      idx -> RegField.bytes(
+        reg,
+        Some(RegFieldDesc(s"SSWI$idx", s"HART index $idx supervisor-level software interrupt device", reset = None))
+      )
+    }: _*
+  )
+
+  /** Each SETSSIP register is a 32-bit wide WARL register where the upper 31 bits are wired to zero. The least
+    * significant bit of a SETSSIP register always reads 0. Writing 0 to the least significant bit of a SETSSIP register
+    * has no effect whereas writing 1 to the least significant bit sends an edge-sensitive interrupt signal to the
+    * corresponding HART causing the HART to set SSIP in the mip CSR. Writes to a SETSSIP register are guaranteed to be
+    * reflected in SSIP of the corresponding HART but not necessarily immediately.
+    */
+  io.sip := VecInit(setssip.map(s => s(0) === 1.U))
 }
